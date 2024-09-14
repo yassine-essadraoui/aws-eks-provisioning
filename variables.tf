8663edba1b52d5b704061415b25dbb12fdd255ee
Yassine <yassine.essadraoui@gmail.com>
2024-09-14 18:39:27 +0200

    Creating an EKS Cluster

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_name" {
+  description = "your vpc name"
+  type        = string
+  default     = ""
+}
+
+variable "cluster_name" {
+  description = "your eks cluster name"
+  type        = string
+  default     = ""
+}
+
+variable "ebs_csi_policy_arn" {
+  description = "AWS managed EBS CSI policy arn"
+  type = string
+  default = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
