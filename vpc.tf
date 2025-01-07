8ebbc2ff61cb675ac6eae4c86ea58a406dcebc11
Yassine ES SADRAOUI <yassine.essadraoui@gmail.com>
2025-01-07 17:01:44 +0100

    Adding variables/outputs needed by other modules

--- vpc.tf
+++ vpc.tf
@@ -4,15 +4,16 @@ module "vpc" {
 
   name = var.vpc_name
 
-  cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  cidr = var.vpc_cidr_range
+
+  azs = slice(data.aws_availability_zones.available.names, 0, 3)
 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
+  enable_nat_gateway   = var.enable_nat_gateway
+  single_nat_gateway   = var.single_nat_gateway
+  enable_dns_hostnames = var.enable_dns_hostnames
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
