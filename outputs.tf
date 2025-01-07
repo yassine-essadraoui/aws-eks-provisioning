4b81a65c8a03be676d4062f961761791d105c72d
Yassine ES SADRAOUI <yassine.essadraoui@gmail.com>
2025-01-07 17:01:44 +0100

    Adding variables/outputs needed by other modules

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,27 @@ output "cluster_access_entries" {
   description = "The Access Entries of the EKS cluster."
   value       = module.eks.access_entries
 }
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "private_subnets_cidr_blocks" {
+  value = module.vpc.private_subnets_cidr_blocks
+}
+
+output "public_subnets_cidr_blocks" {
+  value = module.vpc.public_subnets_cidr_blocks
+}
+
+output "cluster_oidc_issuer_url" {
+  value = module.eks.cluster_oidc_issuer_url
+}
