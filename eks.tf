30cdf4e854447de643d543b82fc254745426c46e
Yassine <yassine.essadraoui@gmail.com>
2025-01-28 16:39:08 +0100

    upgrading module version + eks version + adding oidc output

--- eks.tf
+++ eks.tf
@@ -1,9 +1,9 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.4.0"
+  version = "20.33.1"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+  cluster_version = "1.31"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
--- outputs.tf
+++ outputs.tf
@@ -41,3 +41,11 @@ output "public_subnets_cidr_blocks" {
 output "cluster_oidc_issuer_url" {
   value = module.eks.cluster_oidc_issuer_url
 }
+
+output "oidc_provider_arn" {
+  value = module.eks.oidc_provider_arn
+}
+
+output "oidc_provider" {
+  value = module.eks.oidc_provider
+}
